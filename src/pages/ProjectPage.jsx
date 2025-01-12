3a168320bbb02078fe6f6bf607beb72e34369ff8
mnegash9 <111900361+mnegash9@users.noreply.github.com>
2025-01-11 22:09:04 -0600

    updated project buzz page

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -32,13 +32,15 @@ const ProjectPage = () => {
             </header>
             <main>
                 <div style={{ display: 'flex' }}>
-
-                    <h1 style={{ marginTop: 300, flex: 1, marginLeft:'12px' }} className="text-3xl font-bold mb-4">{project.title}</h1>
+                    <div style={{flex: 1, marginLeft: '12px', marginTop: 300}}>
+                        <h1 className="text-3xl font-bold mb-4">{project.title}</h1>
+                        <em>Share text, files, or images, <br></br> collaborate internally, and <br></br>vote on ideas</em>
+                    </div>
 
                     <div className="projects" style={{ flex: 2, height: "300px", width: "500px", marginTop: "120px", marginLeft: "0px" }}>
                         <div style={{ marginLeft: 30 }}>
                             <p className="text-gray-700">Culmination of a semester long project. Initially privately hosted at Lehigh, it now lives in a Dokku container in an Amazon EC2 instance.</p>
-                            <p>To the right is a system component diagram.</p>
+                            <p>To the right is the system component diagram.</p>
                             <p>It uses a Supabase database, Google OAuth 2.0, Memcachier for caching, and Google Drive services to allow for file and image sharing.</p>
                             <p>The project's emphasis was on following the principles of a 12 factor app as follows: </p>
                             <ol>
@@ -56,17 +58,30 @@ const ProjectPage = () => {
                                 <li>Run admin/management tasks as one-off processes</li>
                             </ol>
                             <p>The Mobile was designed in Flutter, the Web component with HTML/CSS/JS, and the Backend and Admin command line client (for handling database interactions) components were both designed in Java.</p>
-                            <a target="_blank" rel="noopener noreferrer" href="https://matyas.homelinuxserver.org"><u>Link to The Buzz</u></a>
-                            <p>Designed in collaboration with Sean, Monica, Evan, and Mohamed</p>
                         </div>
 
                     </div>
-                    <div style={{ flex: 3, marginLeft:'12px', marginTop:'120px' }} className='project-images'>
+                    <div style={{ flex: 3, marginLeft: '12px', marginTop: '120px' }} className='project-images'>
                         <img src={SystemCompDiagram} className='sticky-image' style={{ maxWidth: '85%' }} />
-                        <img src={ScreenshotUI} className='sticky-image' style={{ maxWidth: '85%', marginTop:'25px'}} />
+                        <img src={ScreenshotUI} className='sticky-image' style={{ maxWidth: '85%', marginTop: '25px' }} />
+                        <section>
+                            <h2>Give it a try here </h2>
+                            <p>The Buzz is an idea sharing app for private internal discussion (like in a company) and allows posting of ideas, comments, and files/images.</p>
+                            <div style={{ display: "flex", flexDirection:"column", justifyContent: "center", textAlign: "center", alignItems: "center" }}>
+                                <div style={{ backgroundColor: "#222831", height: "60px", width: "220px", cursor: "pointer" }}>
+                                    <Link target="_blank" to="https://matyas.homelinuxserver.org" style={{ color: "white", fontSize: 30, margin: "20px" }}>
+                                        <u>The Buzz</u>
+                                    </Link>
+                                </div>
+                                <p>(Designed in collaboration with Sean, Monica, Evan, and Mohamed).</p>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </main>
+            <footer style={{ marginTop: "50px" }}>
+
+            </footer>
         </section >
     );
 };
