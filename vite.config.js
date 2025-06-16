ab34db929531ff495bead8b18ffcb5a6725d6d37
mnegash9 <111900361+mnegash9@users.noreply.github.com>
2025-06-16 02:23:25 -0500

    github action/pages config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteReact from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteReact()],
+  plugins: [react()],
+  server: {
+    open: true,
+  },
   base: "./"
 })
