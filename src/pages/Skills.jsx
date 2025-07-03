390ffa78a89932feab4d1503dc2fdd0bbf5c27c6
Matyas Negash <matyas@mart.marte.gov>
2025-07-03 13:58:34 -0500

    Updates

--- src/pages/Skills.jsx
+++
@@ -1,9 +0,0 @@
-// import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/24/solid";
-import React from "react";
-
-export default function Skills() {
-  return (
-    <section id="skills">
-    </section>
-  );
-}
