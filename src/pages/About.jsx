5e9e8ce83c327bf6fa8149d63518b2770ad47a47
Matyas <matyas@localhost.localdomain>
2025-07-16 13:18:09 -0500

    added to about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams, Link } from 'react-router-dom';
 import Footer from '../components/Footer'
 import ProfilePic from "../assets/profile2.jpeg"
+import Resume from "../assets/resume.pdf";
 
 export default function About() {
   return (
@@ -15,36 +16,40 @@ export default function About() {
           </div>
         </header>
         <main>
-          <div className='about' style={{ display: "flex", marginTop: "120px", overflowY:'auto' }}>
-            <div style={{ marginLeft: "12px", flex: 1 }}>
-              <div className='about-description' style={{  }}>
+          <div className='about' style={{ display: "flex", marginTop: "120px", overflowY: 'auto' }}>
+            <div style={{ marginLeft: "30px", flex: 1 }}>
+              <div className='about-description' style={{}}>
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                   Hi, I'm Matyas.
                   <br className="hidden lg:inline-block" />I like to build things.
                 </h1>
                 <p className="">
-                  I love building things (software + hardware). Some of my past projects include making a
-                  homemade e-bike, working on cars, and replacing the bearing of an LG washer (boo planned obsolescence).
+                  I'm a recent graduate from Lehigh University with a B.S. in Computer Engineering. I have a passion for building things, whether it's hardware or software.
+                  I recently also passed the NCEES FE Exam and will (very soon) obtain my EIT certification. Here is my resume:
                 </p>
-                <p>
-                  Currently, I'm a senior Computer Engineering student at Lehigh University. I'm working with a partner on <Link target="_blank" to="https://terracore-website.onrender.com/"><u>Terracore</u></Link>,
-                  a soil sensing project using the Particle IoT ecosystem.
-                </p>
+                
+                <iframe
+                  src={Resume}
+                  width="860px"
+                  height="1150px"
+                  style={{ border: 'none', marginLeft: '20px', borderRadius: '10px' }}
+                  title="Resume"
+                />
 
               </div>
             </div>
             <div style={{ flex: 1 }}>
-              <div style={{ justifyContent:'center', alignContent:'center'}}>
+              <div style={{ justifyContent: 'center', alignContent: 'center' }}>
                 <img
                   className="avatar"
                   alt="profile"
-                  src={ProfilePic} 
+                  src={ProfilePic}
                 />
               </div>
-              
+
             </div>
           </div>
-          
+
         </main>
         <Footer />
       </section>
