4aa5a5501272a09f861812121e673fc6898e8374
Matyas <matyas@localhost.localdomain>
2025-08-24 21:42:33 -0500

    update + refresh

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams, Link } from 'react-router-dom';
 import Footer from '../components/Footer'
 import ProfilePic from "../assets/profile2.jpeg"
-import Resume from "../assets/resume.pdf";
 
 export default function About() {
   return (
@@ -27,17 +26,17 @@ export default function About() {
                   I'm a recent graduate of Lehigh University with a degree in Computer Engineering. I also recently passed the NCEES FE exam and am awaiting EIT certification. I have held two previous internships, one through Lehigh's Iaccoca Program where I was stationed in Germany working at the ie3 Smart Grid Institute at TU Dortmund.
                   This past summer, I was at Sugar Land Regional Airport where I worked with Microsoft Power Suite to automate and digitize a lot of processes used by the operations agents, and tagged along for daily runway & taxiway inspections, and helped fly drone sweeps after earning my FAA Part 107 Drone certificate (commercial use of drones in the US).
                 </p>
-                <p>
+                {/* <p>
                   I'm currently looking for a new role to use some of my experience and skills at the intersection of software and hardware to better enable the use of technology and improve the human experience.
-                </p>
+                </p> */}
 
-                <iframe
+                {/* <iframe
                   src={Resume}
                   width="860px"
                   height="1150px"
                   style={{ border: 'none', marginLeft: '20px', marginTop: '20px', borderRadius: '10px' }}
                   title="Resume"
-                />
+                /> */}
 
               </div>
             </div>
